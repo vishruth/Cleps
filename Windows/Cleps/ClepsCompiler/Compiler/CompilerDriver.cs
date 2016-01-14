ec1812df2d978d8c46ac99b17536c9c69eb785e8
The SH Developer <the.sh.developer@outlook.com>
2016-01-14 19:32:15 +1300

    Add Compiler driver class and sample code file

---
+++ Windows/Cleps/ClepsCompiler/Compiler/CompilerDriver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClepsCompiler.Compiler
+{
+    class CompilerDriver
+    {
+        public List<string> Files { get; private set; }
+
+        public CompilerDriver(List<string> files)
+        {
+            Files = files;
+        }
+
+        public CompileStatus CompileFiles()
+        {
+            return CompileStatus.GetFailureStatus("", "", "Not implemented");
+        }
+    }
+}
--- Windows/Cleps/ClepsCompiler/Program.cs
+++ Windows/Cleps/ClepsCompiler/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClepsCompiler.Compiler;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,69 @@ namespace ClepsCompiler
 {
     class Program
     {
-        static void Main(string[] args)
+        class ProgramParameters
         {
+            public List<string> Files;
+            public string OutputFile;
+        }
+
+        static int Main(string[] args)
+        {
+            ProgramParameters programParams = ParseParameters(args);
+            if(!ValidateFilesExists(programParams.Files))
+            {
+                return -2;
+            }
+
+            CompilerDriver driver = new CompilerDriver(programParams.Files);
+            CompileStatus status = driver.CompileFiles();
+
+            if(status.Success)
+            {
+                Console.WriteLine("Built successfully");
+            }
+            else
+            {
+                Console.WriteLine(String.Format("Error building\n{0}:{1} {2}", status.ErrorSourceFile, status.ErrorLineNumber, status.ErrorMessage));
+            }
+
+            Console.ReadLine();
+            return 0;
+        }
+
+        private static ProgramParameters ParseParameters(string[] args)
+        {
+            ProgramParameters programParams = new ProgramParameters { Files = new List<string>(args), OutputFile = "outputFile.exe"};
+            if (programParams.Files.Count == 0)
+            {
+                string testFileName = @"..\..\SampleCode\Int32.cleps";
+                string fullPath = Path.GetFullPath(testFileName);
+                if (File.Exists(testFileName))
+                {
+                    programParams.Files.Add(testFileName);
+                }
+            }
+
+            return programParams;
+        }
+
+
+        private static bool ValidateFilesExists(List<string> files)
+        {
+            if(files.Count == 0)
+            {
+                Console.WriteLine("Files to compile are not specified");
+                return false;
+            }
+
+            var missingFiles = files.Where(f => !File.Exists(f));
+            if (missingFiles.Any())
+            {
+                Console.WriteLine("Could not find files : {0}", String.Join(", ", missingFiles));
+                return false;
+            }
+
+            return true;
         }
     }
 }
